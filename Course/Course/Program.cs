2b6f712882877201fc65d9afb37718689f9ec335
Carla Takagi <carlatakagi@gmail.com>
2023-09-17 17:00:11 -0400

    feat: deleted branch

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -19,6 +19,7 @@ namespace Course
             Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Valor de Pi: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
